85e5555bf63ee9383f63cf2a14b76f0813f72674
mr-mbanderson <matthew.bernard.anderson@gmail.com>
2022-11-17 15:22:15 -0500

    Create Remove-CLEANUP_COMPLETE.ps1
    Initial stab at this

---
+++ Remove-CLEANUP_COMPLETE.ps1
@@ -0,0 +1,18 @@
+$computers = Get-Content -Path "<INSERT-PATH-TO-CSV>"
+$ToBeCleaned = @()
+
+# Do these computers have the file CLEANUP_COMPLETE ????
+# If Yes, then add to an array ToBeCleaned
+foreach ($omputer in $computers) {
+  Write-Host ("Checking::"+$computer)
+  $result = Test-Path ("\\"+$computer+"\c$\<PATH-TO-FILE")
+  if ($result -eq $true) {
+    $ToBeCleaned += $computer
+  }
+  Write-Host $ToBeCleaned.Count
+}
+
+# Delete that file CLEANUP_COMPLETE from the array ToBeCleaned
+foreach ($computer in $ToBeCleaned) {
+  Remove-Item -Path ("\\"+$computer+"\c$\<PATH-TO-FILE") -Force
+}
